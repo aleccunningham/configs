ab6dc95209aff96084137ed92b3328a8e4faa96d
Alec Cunningham <aleccunningham96@gmail.com>
2016-11-23 21:02:53 -0600

    add security stuff

--- osx.sh
+++ osx.sh
@@ -9,19 +9,30 @@ defaults write com.apple.finder ShowPathbar -bool true
 echo "Show Status bar in Finder"
 defaults write com.apple.finder ShowStatusBar -bool true
 
+echo "Expose hidden files and Library folder in Finder"
+defaults write com.apple.finder AppleShowAllFiles -bool true
+chflags nohidden ~/Library
+
+echo "Show full filename extensions (i.e. Evil.jpg.app)"
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
 echo "Disable the “Are you sure you want to open this application?” dialog"
 defaults write com.apple.LaunchServices LSQuarantine -bool false
+
+echo "Disable Bonjour multicast advertisements"
+sudo defaults write /Library/Preferences/com.apple.mDNSResponder.plist NoMulticastAdvertisements -bool YES
+
+echo "Disable macOS Captive Portal assistant utility probing unknown networks"
+sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.captive.control Active -bool false
+
+echo "Disable crash report popup"
+defaults write com.apple.CrashReporter DialogType none
 
 echo "Set a blazingly fast keyboard repeat rate"
 defaults write NSGlobalDomain KeyRepeat -int 0.02
 
 echo "Disable auto-correct"
 defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
-
-echo "Disable disk image verification"
-defaults write com.apple.frameworks.diskimages skip-verify -bool true
-defaults write com.apple.frameworks.diskimages skip-verify-locked -bool true
-defaults write com.apple.frameworks.diskimages skip-verify-remote -bool true
 
 echo "Avoid creating .DS_Store files on network volumes"
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
