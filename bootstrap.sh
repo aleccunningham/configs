6c8e7eec6b357636673091183c4efd0b91a799cb
Alec Cunningham <alec@MacPro.local>
2016-11-26 13:37:47 -0600

    update with utilities and organize

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,10 +3,11 @@
 # Execute this file on a clean machine to create the necessary symlinks and
 # copy required files.
 #
-# Requires Dropbox
 # This should be run from the default login shell
-
 set -e
+
+echo "Source utilities for bootstrapping"
+source utils/utils.sh
 
 # Treat unset variables as an error when performing parameter expansion. An
 # error message will be written to the standard error, and a non-interactive
@@ -17,40 +18,60 @@ DOTFILES=$HOME/dotfiles
 CODE=$HOME/Code
 GITHUB=$HOME/Github
 
-mkdir -p $CODE
-mkdir -p $GITHUB
+if [[ ! -d $CODE ]] ; then mkdir -p $CODE
+if [[ ! -d $GITHUB ]] : then mkdir -p $GITHUB
 
-echo "Installing homebrew..."
+e_header "Installing homebrew..."
 ruby -e "$(curl -fsSkL raw.github.com/mxcl/homebrew/go)"
 export PATH=/usr/local/bin:$PATH
 
-echo "Installing all the homebrew things..."
+e_header "Installing all the homebrew things..."
 brew install ack automake cmake git openssl reattach-to-user-namespace ruby tmux unrar wget
+e_success "Done!"
 
-echo "Updating submodules..."
-git submodule init
-git submodule update
-
-echo "Installing Python..."
+e_header "Installing Python..."
 brew install python --framework
+e_success "Done!"
 
-echo "Installing pip and virtualenv..."
+e_header "Installing pip and virtualenv..."
 easy_install pip
 pip install virtualenv virtualenvwrapper fabric pep8 flake8 subliminal pytmux
+e_success "Done!"
 
-echo "Linking rc files"
-ln -s $DOTFILES/vimrc $HOME/.vimrc
+e_header "Creating symlinks"
+sleep 0.5
+
+e_bold "Linking utils file"
+ln -s $DOTFILES/utils/utils.sh $HOME/Library/init/utils.sh
+
+e_bold "Linking vim files"
+ln -s $DOTFILES/vim/vimrc $HOME/.vimrc
 ln -s $DOTFILES/vim $HOME/.vim
-ln -s $DOTFILES/bashrc $HOME/.bashrc
-ln -s $DOTFILES/bash_profile $HOME/.bash_profile
+
+e_bold "Linking bash files"
+ln -s $DOTFILES/bash/bashrc $HOME/.bashrc
+ln -s $DOTFILES/bash/bash_profile $HOME/.bash_profile
 ln -s $DOTFILES $HOME/.profile
 
+e_bold "Linking gitconfigs"
+ln -s $DOTFILES/gitconfig $HOME/.gitconfig
+
+sleep 0.5
+e_success "All dotfiles linked successfully!"
+
+e_bold "Make vim tmp dirs"
 mkdir -p $DOTFILES/vim/tmp/swap
 mkdir -p $DOTFILES/vim/tmp/backup
 mkdir -p $DOTFILES/vim/tmp/undo
 
-# git
-ln -s $DOTFILES/gitconfig $HOME/.gitconfig
+seek_confirmation "Do you want to install node.js?"
+if is_confirmed; then
+  cd $GITHUB && git clone git://github.com/joyent/node.git &&
+  cd $GITHUB/node && ./configure && make && make install &&
+  e_success "Node.js installed!"
+else
+  e_error "Did not install Node.js"
+fi
 
-cd $GITHUB && git clone git://github.com/joyent/node.git
-cd $GITHUB/node && ./configure && make && make install
+# imports all bash settings
+source bash_profile
